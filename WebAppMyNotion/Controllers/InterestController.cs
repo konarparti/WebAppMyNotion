96105947aa18e61861332c7ec3b80662b313724b
Anton <anton.gusevv.01@gmail.com>
2022-02-20 15:52:22 +0300

    add view for data from db

---
+++ WebAppMyNotion/Controllers/InterestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAppMyNotion.Models;
+
+namespace WebAppMyNotion.Controllers
+{
+    public class InterestController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public InterestController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Interests);
+        }
+    }
+}
