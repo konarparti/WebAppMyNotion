22cf23dceb9be47dc7e51ec549fe661337e3d9f6
Anton <anton.gusevv.01@gmail.com>
2022-02-20 15:16:11 +0300

    initial commit

---
+++ WebAppMyNotion/Models/Interest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAppMyNotion.Models
+{
+    public class Interest
+    {
+        public Interest() => DateAdded = DateTime.Now;
+        
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Название")]
+        public string Name { get; set; }
+        [Display(Name = "Тип")]
+        public string? Type { get; set; }
+        [Display(Name = "Область")]
+        public string? Scope { get; set; }
+        [Display(Name = "Статус")]
+        public string? Status { get; set; }
+        [Display(Name = "Прогресс")]
+        public string? Progress { get; set; }
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Дата добавления")]
+        public DateTime DateAdded { get; set; }
+    }
+}
