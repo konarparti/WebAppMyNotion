a9391117ee58ff842ba4d41ca603c43a2eca037f
Anton <anton.gusevv.01@gmail.com>
2022-02-23 17:44:08 +0300

    add table search

---
+++ WebAppMyNotion/wwwroot/js/table.js
@@ -0,0 +1,18 @@
+﻿const tableSearch = () => {
+    const phrase = document.getElementById("search");
+    const table = document.getElementById("main-table");
+    const regPhrase = new RegExp(phrase.value, "i");
+    let flag;
+    for (let i = 1; i < table.rows.length; i++) {
+        flag = false;
+        for (let j = table.rows[i].cells.length - 1; j >= 0 ; j--) {
+            flag = regPhrase.test(table.rows[i].cells[j].innerHTML);
+            if (flag) break;
+        }
+        if (flag) {
+            table.rows[i].style.display = "";
+        } else {
+            table.rows[i].style.display = "none";
+        }
+    }
+}
