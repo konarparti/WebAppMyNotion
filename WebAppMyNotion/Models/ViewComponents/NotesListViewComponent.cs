9a70e54e41eb588f7e2b6bbdff45c314a62abdbf
Anton <anton.gusevv.01@gmail.com>
2022-05-04 18:41:28 +0300

    add view component for links

---
+++ WebAppMyNotion/Models/ViewComponents/NotesListViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppMyNotion.Models.ViewComponents
+{
+    public class NotesListViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NotesListViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            return Task.FromResult((IViewComponentResult)View("Default", _context.Notes.ToList()));
+        }
+    }
+}
